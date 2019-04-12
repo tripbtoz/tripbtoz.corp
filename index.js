d1941913765a7661276b4c5b1cde0ea5d49d631d
michaelpark77 <teletype777@gmail.com>
2019-04-12 11:54:24 +0900

    minor change

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var width = $(window).width()
+var height = $(window).height()
 $(".menu").click(function(){
   var target = $(this).attr('data-target')
   var scrollTop = $("#"+target).offset().top
@@ -28,12 +29,8 @@ $('.modal-value-button').click(function () {
   var to = button.attr('data-value')
   var page = $('#modal-value-page').val()
   page = parseInt(page)
-  console.log(page)
-  
   if (to === 'prev' && page > 0) {
-    console.log('aaaa')
     page -= 1
-    
   } else if (to === 'next' && page < 2) {
     page += 1
   }
@@ -43,7 +40,7 @@ $('.modal-value-button').click(function () {
   $('#modal-value-content').css({marginLeft: marginLeftValue})
 })
 $(window).on('scroll', function () {
-  var threshold = 200
+  
   var windowScrollTop = $(window).scrollTop()
   
   
@@ -56,63 +53,29 @@ $(window).on('scroll', function () {
   var joinTop = $("#join").offset().top
   var contactTop = $("#contact").offset().top
 
-  var ambitionBottom = $("#ambition").offset().top + $('#ambition').height()
-  var hotelServiceBottom = $("#hotel-service").offset().top + $("#hotel-service").height()
-  var visionBottom = $("#vision").offset().top + $("#vision").height()
-  var definitionBottom = $("#definition").offset().top + $("#definition").height()
-  var valueBottom = $("#value").offset().top + $("#value").height()
-  var careerBottom = $("#career").offset().top + $("#career").height()
-  var joinBottom = $("#join").offset().top + $("#join").height()
-  var contactBottom = $("#contact").offset().top + $('#contact').height()
+  // var ambitionBottom = $("#ambition").offset().top + $('#ambition').height()
+  // var hotelServiceBottom = $("#hotel-service").offset().top + $("#hotel-service").height()
+  // var visionBottom = $("#vision").offset().top + $("#vision").height()
+  // var definitionBottom = $("#definition").offset().top + $("#definition").height()
+  // var valueBottom = $("#value").offset().top + $("#value").height()
+  // var careerBottom = $("#career").offset().top + $("#career").height()
+  // var joinBottom = $("#join").offset().top + $("#join").height()
+  // var contactBottom = $("#contact").offset().top + $('#contact').height()
   
   
-  if (ambitionTop - threshold < windowScrollTop && windowScrollTop < ambitionBottom + threshold){
-    $('#ambition').addClass('active')
-  } else {
-    $('#ambition').removeClass('active')
-  }
-
-  if (hotelServiceTop - threshold < windowScrollTop && windowScrollTop < hotelServiceBottom + threshold){
-    $('#hotel-service').addClass('active')
-  } else {
-    $('#hotel-service').removeClass('active')
-  }
-
-  
-  if (visionTop - threshold < windowScrollTop && windowScrollTop < visionBottom + threshold){
-    $('#vision').addClass('active')
-  } else {
-    $('#vision').removeClass('active')
-  }
-
-  if (definitionTop - threshold < windowScrollTop && windowScrollTop < definitionBottom + threshold){
-    $('#definition').addClass('active')
-  } else {
-    $('#definition').removeClass('active')
-  }
-
-  if (valueTop - threshold < windowScrollTop && windowScrollTop < valueBottom + threshold){
-    $('#value').addClass('active')
-  } else {
-    $('#value').removeClass('active')
-  }
-
-  if (careerTop - threshold < windowScrollTop && windowScrollTop < careerBottom + threshold){
-    $('#career').addClass('active')
-  } else {
-    $('#career').removeClass('active')
-  }
-  
-  if (joinTop - threshold < windowScrollTop && windowScrollTop < joinBottom + threshold){
-    $('#join').addClass('active')
-  } else {
-    $('#join').removeClass('active')
-  }
-  
-  if (contactTop - threshold < windowScrollTop && windowScrollTop < contactBottom + threshold){
-    $('#contact').addClass('active')
-  } else {
-    $('#contact').removeClass('active')
-  }
-
+  // testVisibleAndShow(windowScrollTop, ambitionTop, "#ambition")
+  testVisibleAndShow(windowScrollTop, hotelServiceTop, "#hotel-service")
+  testVisibleAndShow(windowScrollTop, visionTop, "#vision")
+  testVisibleAndShow(windowScrollTop, definitionTop, "#definition")
+  testVisibleAndShow(windowScrollTop, valueTop, "#value")
+  testVisibleAndShow(windowScrollTop, careerTop, "#career")
+  testVisibleAndShow(windowScrollTop, joinTop, "#join")
+  testVisibleAndShow(windowScrollTop, contactTop, "#contact")
 })
+function testVisibleAndShow (windowScrollTop, itemTop, selector) {
+  if (windowScrollTop < itemTop - 150 && itemTop + 150 < windowScrollTop + height ) {
+    $(selector).addClass('active')
+  } else {
+    $(selector).removeClass('active')
+  }
+}
