0097c06ff7edaa4e495c4f99445e9d4e2ff46dc3
michaelpark77 <teletype777@gmail.com>
2019-05-09 17:54:37 +0900

    fixes by comments from CEO

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
 var width = $(window).width()
 var height = $(window).height()
-$(".menu").click(function(){
-  var target = $(this).attr('data-target')
-  var scrollTop = $("#"+target).offset().top
-  if (window.width < 768) {
-    scrollTop -= 72
-  } else {
-    scrollTop -= 92
-  }
-  $('html,body').animate({
-    'scrollTop': scrollTop
-  }, 500)
+$(window).on('resize', function () {
+  width = $(window).width()
+  height = $(window).height()
 })
-$('.button').click(function(){
-  var button = $(this)
-  var target = button.attr('data-modal-target');
-  $("#modal-content > *").hide();
-  $("#"+target).show();
-  $("#modal").addClass('show')
-  $('body').css({'overflow': 'hidden'})
-})
-$('#modal-close-btn').click(function () {
-  $('body').css({'overflow': 'auto'})
-  $("#modal").removeClass('show')
-})
-$('.modal-value-button').click(function () {
-  var button = $(this)
-  var to = button.attr('data-value')
-  var page = $('#modal-value-page').val()
-  page = parseInt(page)
-  if (to === 'prev' && page > 0) {
-    page -= 1
-  } else if (to === 'next' && page < 2) {
-    page += 1
-  }
-  $("#modal-value-page-ind").html(page+1)
-  $('#modal-value-page').val(page)
-  var marginLeftValue = (- page * 100)+'%'
-  $('#modal-value-content').css({marginLeft: marginLeftValue})
+console.log(width, height)
+$(window).ready(function () {
+  $(".menu").click(function(){
+    var target = $(this).attr('data-target')
+    var scrollTop = $("#"+target).offset().top
+    if (window.width < 768) {
+      scrollTop -= 100
+    } else {
+      scrollTop -= 92
+    }
+    $('html,body').animate({
+      'scrollTop': scrollTop
+    }, 500)
+  })
+  $('.button').click(function(){
+    var button = $(this)
+    var target = button.attr('data-modal-target');
+    $("#modal-content > *").hide();
+    $("#"+target).show();
+    $("#modal").addClass('show')
+    $('body').css({'overflow': 'hidden'})
+  })
+  $('#modal-close-btn').click(function () {
+    $('body').css({'overflow': 'auto'})
+    $("#modal").removeClass('show')
+  })
+  $('.modal-value-button').click(function () {
+    var button = $(this)
+    var to = button.attr('data-value')
+    var page = $('#modal-value-page').val()
+    page = parseInt(page)
+    if (to === 'prev' && page > 0) {
+      page -= 1
+    } else if (to === 'next' && page < 2) {
+      page += 1
+    }
+    $("#modal-value-page-ind").html(page+1)
+    $('#modal-value-page').val(page)
+    var marginLeftValue = (- page * 100)+'%'
+    $('#modal-value-content').css({marginLeft: marginLeftValue})
+  })
+
 })
 $(window).on('scroll', function () {
   
@@ -53,6 +61,14 @@ $(window).on('scroll', function () {
   var joinTop = $("#join").offset().top
   var contactTop = $("#contact").offset().top
 
+  var hotelServiceHeight = $("#hotel-service").height()
+  var visionHeight = $("#vision").height()
+  var definitionHeight = $("#definition").height()
+  var valueHeight = $("#value").height()
+  var careerHeight = $("#career").height()
+  var joinHeight = $("#join").height()
+  var contactHeight = $("#contact").height()
+
   // var ambitionBottom = $("#ambition").offset().top + $('#ambition').height()
   // var hotelServiceBottom = $("#hotel-service").offset().top + $("#hotel-service").height()
   // var visionBottom = $("#vision").offset().top + $("#vision").height()
@@ -64,16 +80,27 @@ $(window).on('scroll', function () {
   
   
   // testVisibleAndShow(windowScrollTop, ambitionTop, "#ambition")
-  testVisibleAndShow(windowScrollTop, hotelServiceTop, "#hotel-service")
-  testVisibleAndShow(windowScrollTop, visionTop, "#vision")
-  testVisibleAndShow(windowScrollTop, definitionTop, "#definition")
-  testVisibleAndShow(windowScrollTop, valueTop, "#value")
-  testVisibleAndShow(windowScrollTop, careerTop, "#career")
-  testVisibleAndShow(windowScrollTop, joinTop, "#join")
-  testVisibleAndShow(windowScrollTop, contactTop, "#contact")
+  testVisibleAndShowForHotelService(windowScrollTop, hotelServiceTop, hotelServiceHeight,  "#hotel-service")
+  testVisibleAndShow(windowScrollTop, visionTop, visionHeight, "#vision")
+  testVisibleAndShow(windowScrollTop, definitionTop, definitionHeight, "#definition")
+  testVisibleAndShow(windowScrollTop, valueTop, valueHeight, "#value")
+  testVisibleAndShow(windowScrollTop, careerTop, careerHeight, "#career")
+  testVisibleAndShow(windowScrollTop, joinTop, joinHeight, "#join")
+  testVisibleAndShow(windowScrollTop, contactTop, contactHeight, "#contact")
 })
-function testVisibleAndShow (windowScrollTop, itemTop, selector) {
-  if (windowScrollTop < itemTop - 150 && itemTop + 150 < windowScrollTop + height ) {
+function testVisibleAndShowForHotelService (windowScrollTop, itemTop, itemHeight, selector) {
+  // 화면의 최상단 => windowScrollTop
+  // 화면의 최하단 => windowScrollTop + height
+  if (windowScrollTop  < itemTop && itemTop  < windowScrollTop + height - 150) {
+    $(selector).addClass('active')
+  } else {
+    $(selector).removeClass('active')
+  }
+}
+function testVisibleAndShow (windowScrollTop, itemTop, itemHeight, selector) {
+  // 화면의 최상단 => windowScrollTop
+  // 화면의 최하단 => windowScrollTop + height
+  if (windowScrollTop  < itemTop && itemTop + itemHeight - 150 < windowScrollTop + height - 150) {
     $(selector).addClass('active')
   } else {
     $(selector).removeClass('active')
